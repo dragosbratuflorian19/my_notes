2ff4a920b3a5bc07966f6dbf7debc2402425032e
dragosbratuflorian19 <dragosbratuflorian19@gmail.com>
2020-05-01 00:48:48 +0300

    dsada

--- try.py
+++ try.py
@@ -1,1 +1,5 @@
-print(2**100)
+her = 5000
+me = 10000
+total = ( her + me) * 0.85 * 24
+print(f"Suma pe care o vei strange tu cu minunata ta iubita programatoare in 2 ani va fi:{total}")
+print(f"cu cat traiti pe luna:{total * 0.15 / 24}")
