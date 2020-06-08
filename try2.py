5e975a2691aa0d6c766f22eaa545b8d180ab019b
dragosbratuflorian19 <dragosbratuflorian19@gmail.com>
2020-06-09 00:02:54 +0300

    'sda'

--- try2.py
+++ try2.py
@@ -1,23 +1,5 @@
-How code is run in python?
-Byte code compilation?
-What is The Python Virtual Machine (PVM) ?
-What are Frozen Binaries?
-How do you store the output of a script?
-Turn python files into executable programs.
-What is Polymorphism?
-Declare a string as a bytearray.
-Print a number with % 
-Declare a raw string.
-Encode a string in 2 different utfs.
-What is Garbage collection?
-How do you find out how many references an object has?
-Make some strings formatting: '1.23 | 01.23 | +001.2', '       ### = X         ', '3.141590e+00, 3.142e+00, 3.14159, 003.14', '296,999.26'
-Use map
-Merge 2 dictionaries
-Use named tuple
-Use pickle and json
-Use assertion
-Use lambda and reduce
-Use Pseudo_private variables
-Use Bound or Unbound variables
-Use object factories
+from pyspark import SparkContext, SparkConf
+
+conf = SparkConf().setAppName("word count").setMaster("local")
+sc = SparkContext(conf=conf)
+rdd = sc.parallelize(range(1, 7))
